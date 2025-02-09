00d9390e9246b52080f1416157a0054386e5cf68
Jayas Piya <pzayazz@gmail.com>
2025-02-09 06:14:49 +0545

    Added Streamlit Base Dashboard

---
+++ streamlit/pages/main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Patient Readmission Model")
+st.header("Predictive Model vs LACE Index")
+st.text("Comparision between predictive model & lace index accuracy")
